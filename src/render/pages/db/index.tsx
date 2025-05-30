724c1704fd0b76d3f437e5cb13ef6bc63a7cfb55
handeqin <handeqin@chinatelecom.cn>
2025-05-30 23:06:29 +0800

    数据维护

--- src/render/pages/db/index.tsx
+++ src/render/pages/db/index.tsx
@@ -1,75 +1,83 @@
 import { sqDelete, sqInsert, sqQuery, sqUpdate, openFile, exportToExcel } from '../../../common/db'
 import { Button, Space, Table, Tabs } from 'antd'
-import { AndroidOutlined, AppleOutlined } from '@ant-design/icons';
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import type { TableProps,TabsProps  } from 'antd';
 
+
 interface DataType {
-    key: string;
-    name: string;
-    age: number;
-    address: string;
-    tags: string[];
+    id: number;
+    label: string;
+    value: number;
 }
 
-
-
 const DBPage = () => {
-    const [activeKey, setActiveKey] = React.useState<string>('1');
+    const [activeKey, setActiveKey] = React.useState<string>('pcba');
+    const [data, setData] = React.useState<DataType[]>([]);
+
+    useEffect(() => {
+        queryHandle(activeKey);
+    }, []);
 
     const columns: TableProps<DataType>['columns'] = [
         {
+            title: 'ID',
+            dataIndex: 'id',
+            align:'center',
+          },
+        {
             title: '名称',
-            dataIndex: 'name',
-            key: 'name',
+            dataIndex: 'label',
+            key: 'label',
+            align:'center',
             render: (text) => <a>{text}</a>,
         },
         {
             title: '值',
+            align:'center',
             dataIndex: 'value',
             key: 'value',
         },
     ];
-    const data: DataType[] = []
+
     const items:TabsProps['items'] = [
         {
             key: 'pcba',
             label: `PCBA厂数据`,
-            children: <Table<DataType> columns={columns} dataSource={data} />,
+            children: <Table<DataType> rowKey={'id'} bordered  columns={columns} dataSource={data} />,
         },
         {
             key: 'category',
             label: `产品类别`,
-            children: <Table<DataType> columns={columns} dataSource={data} />,
+            children: <Table<DataType> rowKey={'id'} columns={columns} dataSource={data} />,
         },
         {
             key: 'specifications',
             label: `产品规格`,
-            children: <Table<DataType> columns={columns} dataSource={data} />,
+            children: <Table<DataType> rowKey={'id'} columns={columns} dataSource={data} />,
         },
         {
             key: 'series',
             label: `产品代系`,
-            children: <Table<DataType> columns={columns} dataSource={data} />,
+            children: <Table<DataType> rowKey={'id'} columns={columns} dataSource={data} />,
         },
         {
             key: 'productionId',
             label: `产品序列号`,
-            children: <Table<DataType> columns={columns} dataSource={data} />,
+            children: <Table<DataType> rowKey={'id'} columns={columns} dataSource={data} />,
         },
     ]
     const sqInsertHandle = (table: string, data: any) => {
-        sqInsert({table: table, data: data}).then((res: any) => {
-            console.log(res)
+        sqInsert({table: table, data: data}).then(() => {
+            queryHandle(activeKey)
         })
     }
 
-    const queryHandle = () => {
+    const queryHandle = (tableName: string) => {
         sqQuery({ 
-            sql: `SELECT * FROM ${activeKey}`,
+            sql: `SELECT * FROM ${tableName}`,
             params: []
         }).then((res: any) => {
-            console.log(res)
+            setData(res)
         })
     }
 
@@ -109,8 +117,12 @@ const DBPage = () => {
     }
     const handleOpenFile = async () => {
         openFile().then((res)=> {
-            const data = res.map((item: any) => ({label: item[0], value: item[1]}));
-            sqInsertHandle(activeKey, data)
+            if (res && res.length>0){
+                const data = res.map((item: any) => ({label: item[0], value: item[1]}));
+               for(let key of data){
+                sqInsertHandle(activeKey, key)
+               }
+            }
         }).catch((err) => {
             console.error("Error opening file:", err);
         });
@@ -118,31 +130,20 @@ const DBPage = () => {
 
     const deleteHandle = () => {
         //  condition: 'name = "a"'
-        sqDelete({table: activeKey,condition:''}).then((res: any) => {
-            console.log(res)
+        sqDelete({table: activeKey}).then(() => {
+            queryHandle(activeKey)
         })
     }
 
     const handleTabChange = (key: string) => {
-        console.log(key);
         setActiveKey(key);
-
-        queryHandle()
+        setData([]);
+        queryHandle(key)
     };
 
     const operations = <Space><Button type="primary" onClick={handleOpenFile}>上传</Button><Button type="primary" onClick={deleteHandle}>删除</Button></Space>;
     return <div>
-        {/* <Space>
-            <Button onClick={sqInsertHandle}>增加数据</Button>
-            <Button onClick={queryHandle}>查询数据</Button>
-            <Button onClick={updateHandle}>更新数据</Button>
-            <Button onClick={deleteHandle}>删除数据</Button>
-
-            <Button onClick={exportToFile}>导出</Button>
-            <Button onClick={handleOpenFile}>PCBA厂数据</Button>
-        </Space> */}
-
-        <Tabs defaultActiveKey="pcba" onChange={handleTabChange} activeKey={activeKey} tabBarExtraContent={operations} items={items} />
+        <Tabs defaultActiveKey="pcba"  type="card" size="large" onChange={handleTabChange} activeKey={activeKey} tabBarExtraContent={operations} items={items} />
     </div>
 }
 
